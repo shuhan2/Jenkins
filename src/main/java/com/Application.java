8f0d034181d7ea1672ea9c4ea80eee2578e0b07e
qianwu2 <xinyuan@thoughtworks.com>
2018-11-28 23:44:16 +0800

    Jenkins[wuqian]add a new branch to test jenkins

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -8,6 +8,7 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("first");
+        System.out.println("new");
         SpringApplication.run(Application.class, args);
 
     }

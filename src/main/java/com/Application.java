76e9b7adca7608c9d839dfe979059a9259b770ea
qianwu2 <xinyuan@thoughtworks.com>
2018-11-29 08:41:46 +0800

    Jenkins[wuqian]add a new branch to test jenkins

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+
         System.out.println("first");
         System.out.println("new");
         SpringApplication.run(Application.class, args);

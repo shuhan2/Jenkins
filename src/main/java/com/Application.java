d408c44de88b28f074380abb86c877618d2a971c
qianwu2 <xinyuan@thoughtworks.com>
2018-12-02 15:25:16 +0800

    Jenkins[wuqian]remove some output string to test jenkins

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("first");
         SpringApplication.run(Application.class, args);
 
     }
